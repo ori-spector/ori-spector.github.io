5eb3ebbca2d9ecec10e03fc45ee4f1596b70ce55
ori <100273282+ori-spector@users.noreply.github.com>
2026-05-10 22:16:02 -0700

    Update list item content in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -322,7 +322,7 @@ document.addEventListener('DOMContentLoaded', () => {
             content: `
                 <ul>
                     <li><strong>Mediums matter.</strong> The visual domain is the most powerful alignment tool we have.</li>
-                    <li><strong>New interfaces are necessary.</strong> Antiquated technologies are disrupted when new tools run freely.</li>
+                    <li><strong>New experiences are necessary.</strong> Disruption is caused by letting the frontier run freely.</li>
                     <li><strong>Nature, media, and art ground us.</strong> We need to use them intentionally.</li>
                 </ul>
             `
